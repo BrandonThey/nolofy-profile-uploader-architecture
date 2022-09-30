633282bbef0e264decfd803f7b575574eb1d8611
Rola <cloudgit.241c2@simplelogin.co>
2022-09-30 08:17:53 -0500

    working db aws architecture

--- main.tf
+++ main.tf
@@ -1,72 +1,97 @@
 provider "aws" {
-  region = "us-east-1" //CHANGE THIS IF NEEDED
+  region = "us-west-1"
 }
 
-//Creating a vpc with a unique 16 bit mask
-//This vpc will house our subnets for the app and the database
-//This will also create a private route table for our database
-resource "aws_vpc" "nology-uploader-vpc" {
-  cidr_block = "99.0.0.0/16" //CHANGE THIS LATER IF NEEDED
+resource "aws_vpc" "rolan-vpc-project" {
+  cidr_block = "10.0.0.0/16"
+
   tags = {
-    Name = "nology-uploader-vpc"
+    Name = "rolan-vpc-project"
   }
 }
 
-//Creating Internet gateway that will connect our VPC
-//and contents inside with the internet
-resource "aws_internet_gateway" "nology-uploader-ig" {
-  vpc_id = "${aws_vpc.nology-uploader-vpc.id}"
+resource "aws_internet_gateway" "rolan-ig" {
+  vpc_id = "${aws_vpc.rolan-vpc-project.id}"
+  
   tags = {
-    Name = "nology-uploader-ig"
+    Name = "rolan-ig"
   }
 }
 
-//Creating our public route table that will connect the internet gateway
-//with our public app
-resource "aws_route_table" "nology-uploader-rt" {
-  vpc_id = "${aws_vpc.nology-uploader-vpc.id}"
-  route {
+resource "aws_route_table" "rolan-rt" {
+  vpc_id = "${aws_vpc.rolan-vpc-project.id}"
+
+ route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.nology-uploader-ig.id}"
-  }
-  tags = {
-    Name = "nology-uploader-rt"
+    gateway_id = "${aws_internet_gateway.rolan-ig.id}"
+ } 
+
+ tags = {
+   Name = "rolan-rt"
+ }
+}
+
+resource "aws_ami" "rolan-ami" {
+  name                = "mysql-ami"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/sda1"
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    snapshot_id    = "snap-01f4170721db0d703"
   }
 }
 
-//Module for the database, it will pass on variables to the database 
-//terraform files
+# resource "aws_ebs_volume" "example" {
+#   availability_zone = "us-west-2a"
+#   size              = 40
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+
+# resource "aws_ebs_snapshot" "example_snapshot" {
+#   volume_id = aws_ebs_volume.example.id
+
+#   tags = {
+#     Name = "HelloWorld_snap"
+#   }
+# }
+
 module "db-tier" {
-  name="nology-uploader-db"
-  source = "./modules/mysql-db"
-  vpc_id = "${aws_vpc.nology-uploader-vpc.id}"
-  route_table_id = "${aws_vpc.nology-uploader-vpc.main_routing_table}"
-  cidr_block = "99.0.1.0/24"
-  user_data = templatefile("./scripts/db_user_data.sh", {}) //NEED TO WORK ON GETTING SCRIPT FOR THIS
-  ami_id = "ami-0c39c5647431f40ef" //NEEDS TO BE CHANGED LATER BASED ON REGION AND IMAGE, TO MATCH BACKEND MACHINE
-
-  ingress = [{
-    from_port = 3306 //default mysql port
-    to_port = 3306
-    protocol = "tcp"
-    cidr_block = "${module.app-tier.subnet_cidr_block}"
-  }]
-}
-
-module "app-tier" {
-  name = "nology-uploader-app"
-  source = "./modules/app"
-  vpc_id = "${aws_vpc.nology-uploader-vpc.id}"
-  route_table_id = "${aws_vpc.nology-uploader-rt.id}"
-  cidr_block = "99.0.2.0/24"
-  user_data = templatefile("./scripts/app_user_data.sh", {/*mongodb_ip = module.db-tier.private_ip*/}) //NEED TO WORK ON GETTING SCRIPT FOR THIS
-  ami_id = "ami-0c39c5647431f40ef" //NEEDS TO BE CHANGED LATER BASED ON REGION AND IMAGE, TO MATCH FRONTEND MACHINE
+  name="rolan-db"
+  source="./modules/db-tier" #looks for a main tf at that path
+  vpc_id="${aws_vpc.rolan-vpc-project.id}"
+  route_table_id = "${aws_route_table.rolan-rt.id}"
+  cidr_block="10.0.10.0/24"
+  user_data=templatefile("./scripts/db_user_data.sh", {})
+  ami_id = "${aws_ami.rolan-ami.id}"
   map_public_ip_on_launch = true
+  
 
   ingress = [{
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_block = "0.0.0.0/0"
+    from_port       = 3024
+    to_port         = 3024
+    protocol        = "tcp"
+    cidr_blocks     = "0.0.0.0/0"  //"${module.application-tier.subnet_cidr_block}"
   }]
 }
+
+# module "application-tier" {
+#   name="rolan-application"
+#   source="./modules/application-tier" #looks for a main tf at that path
+#   vpc_id="${aws_vpc.rolan-vpc-project.id}"
+#   route_table_id = "${aws_route_table.rolan-rt.id}"
+#   cidr_block="10.0.11.0/24"
+#   user_data=templatefile("./scripts/app_user_data.sh", {mongodb_ip = module.db-tier.private_ip})
+#   ami_id = "ami-0cb88e06cf1447bd6"
+#   map_public_ip_on_launch = true
+
+#   ingress = [{
+#     from_port       = 80
+#     to_port         = 80
+#     protocol        = "tcp"
+#     cidr_blocks = "0.0.0.0/0"
+#   }]
+# }
